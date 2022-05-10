ec2b982eda47c969a6e2f4d6bc691be15c32bb43
manasikattel <manasikattel1@gmail.com>
2022-05-11 00:22:46 +0200

    update bbox format

--- dead_cells/dead_cells_bboxes.py
+++ dead_cells/dead_cells_bboxes.py
@@ -17,7 +17,6 @@ output_path = Path(
 output_path.mkdir(exist_ok=True)
 
 
-bbox = {"cell_name": [], "x_min": [], "y_min": [], "x_max": [], "y_max": []}
 for img, img_name in dead_images_raw:
     circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 10, minDist=25,
                                param1=100, param2=0.1, minRadius=7,
@@ -25,20 +24,28 @@ for img, img_name in dead_images_raw:
                                )
 
     output = img.copy()
-    print(circles.shape)
 
     if circles is not None:
+        bbox = {"cell_name": [], "x_min": [],
+                "y_min": [], "x_max": [], "y_max": []}
+
         circles = np.round(circles[0, :]).astype("int")
         for (x, y, r) in circles:
+            print((x, y, r))
+            # cv2.circle(output, (x, y), r+5, (0, 255, 0), 2)
             cv2.rectangle(output, (x - r - 5, y - r - 5),
                           (x + r + 5, y + r + 5), (255, 0, 0), 2)
-            bbox["cell_name"].append(img_name)
+            bbox["cell_name"].append("dead")
+
             bbox["x_min"].append(x - r - 5)
             bbox["y_min"].append(y - r - 5)
             bbox["x_max"].append(x + r + 5)
             bbox["y_max"].append(y + r + 5)
+        bboxes = pd.DataFrame(bbox)
+        filename = img_name.split(".")[0]
+        bboxes[["cell_name", "x_min", "y_min", "x_max", "y_max"]].to_csv(
+            str(output_path/Path(f"{filename}.txt")), header=None, index=None)
 
+        # cv2.imshow("output", np.hstack([img, output]))
         cv2.imwrite(str(output_path / img_name), np.hstack([img, output]))
-
-bboxes = pd.DataFrame(bbox)
-bboxes.to_csv(str(output_path/Path("bboxes.txt")), header=None, index=None)
+        # cv2.waitKey(0)
